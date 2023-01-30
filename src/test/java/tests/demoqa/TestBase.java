23d4f7ca1c1191ab61091701e9a1e480bbc51a52
Aleksey Volosovich <aleksey.volosovich@quadcode.com>
2023-01-30 21:57:37 +0200

    Add selenoid

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -18,10 +18,10 @@ public class TestBase {
     @BeforeAll
     static void setUp() {
         Configuration.browserSize = "1920x1080";
-     //   Configuration.browser = "firefox";
-//        Configuration.browserVersion = "97.0";
+        Configuration.browser = "firefox";
+        Configuration.browserVersion = "97.0";
         Configuration.baseUrl = "https://demoqa.com";
-    //    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
